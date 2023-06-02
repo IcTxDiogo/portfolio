b924aa8aba3fe4bfad8004729eae5ed48cc1eae9
Diogo França dos Santos <ictxdiogo@gmail.com>
2023-06-02 14:37:23 -0400

    add domains

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['avatars.githubusercontent.com', 'i.ibb.co', 'i.imgur.com'],
+  },
+}
 
 module.exports = nextConfig
